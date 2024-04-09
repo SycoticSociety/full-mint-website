f22ba708c9211be067cadde4902fc26bd101ee40
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-04-09 02:19:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
   const [accounts, setAccounts] = useState([]);
 
   return (
-  <div className="App">
-    <NavBar accounts={accounts} setAccounts={setAccounts} />
-    <MainMint accounts={accounts} setAccounts={setAccounts} />
-  </div>
-  
+    <div className="App">
+      <NavBar accounts={accounts} setAccounts={setAccounts} />
+      <MainMint accounts={accounts} setAccounts={setAccounts} />
+    </div>
   );
 }
+
+export default App;
