61100e2a36b6f4bc9568bb71d1b8ebeb4d90bc6b
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-04-09 02:32:17 -0400

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,38 +1,45 @@
 import React from 'react';
 
-const NavBar = ({accounts, setAccounts}) => {
+const NavBar = ({ accounts, setAccounts }) => {
     const isConnected = Boolean(accounts[0]);
 
     async function connectAccount() {
         if (window.ethereum) {
-            const accounts = await window.ethereum.request.request({
-                method: "eth_requestAccounts" ,
-            
-            });
-            setAccounts(accounts);
+            try {
+                const accounts = await window.ethereum.request({
+                    method: "eth_requestAccounts",
+                });
+                setAccounts(accounts);
+            } catch (error) {
+                console.error("Error connecting to MetaMask", error);
+                // Optionally, provide user feedback here (e.g., a toast notification)
+            }
+        } else {
+            console.log("Please install MetaMask!");
+            // Optionally, provide user feedback here (e.g., an alert or modal suggesting to install MetaMask)
         }
     }
 
     return (
         <div>
-         {/*Left Side - Social Media Icons */}
+            {/*Left Side - Social Media Icons */}
+            <div>Facebook</div>
+            <div>Twitter</div>
+            <div>Email</div>
 
-        <div>Facebook</div>
-        <div>Twitter</div>
-        <div>Email</div>
+            {/*Right Side - Sections and Connect */}
+            <div>About</div>
+            <div>Mint</div>
+            <div>Coming-Soon</div>
 
-        {/*Right Side - Sections and Connect */}
-        <div>About</div>
-        <div>Mint</div>
-        <div>Coming-Soon</div>
-
-        {/*Connect*/}
-        {isConnected ? (
-            <p>Connected</p>
-        ) : (
-            <button onClick={connectAccount}>Connect</button>
-        )}
+            {/*Connect*/}
+            {isConnected ? (
+                <p>Connected</p>
+            ) : (
+                <button onClick={connectAccount}>Connect</button>
+            )}
         </div>
     );
 };
+
 export default NavBar;
